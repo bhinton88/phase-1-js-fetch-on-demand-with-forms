5804bf3e2ae55716947bea6e3b56b58dc4997df1
Bobby Hinton <bob.g.hint@gmail.com>
2022-06-10 23:51:18 -0500

    commit tree f29175994acbd004533dbb78500d6ddc09cd2357

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
+  const inputForm = document.querySelector('form'); 
   
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    // prevent default allows us to keep the form from refreshing which is a default propery
+    // event.target.children[1].value also works for below code 
+    const input = document.querySelector('input#searchByID')
+
+    console.log(input.value)
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+      const title = document.querySelector('section#movieDetails h4')
+      const summary = document.querySelector('section#movieDetails p')
+      
+      title.textContent = data.title;
+      summary.textContent = data.summary; 
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
